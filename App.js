154ac46fa231ac8c5551e2c215a2da0ab45ca073
azeez1776 <aboud3am81@gmail.com>
2021-09-17 16:41:44 +0300

    react native movie app

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DetailsScreen from './src/screens/DetailsScreen';
+import ImageScreen from './src/screens/ImageScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+        />
+        <Stack.Screen
+          name="Image"
+          component={ImageScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
