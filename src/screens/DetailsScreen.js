1993531a05caaa1b0bdc1307bdb4f8ba9e7f726b
azeez1776 <aboud3am81@gmail.com>
2021-09-19 14:51:43 +0300

    added useState hook so as to assign the variables to the response of the API and use them in the Text component

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 const DetailsScreen = ({ navigation, route }) => {
   const { movie } = route.params
 
+  const [movieDetail, setMovieDetail] = useState('');
   useEffect(() => {
     const muvi = async () => {
       try {
         const data = await fetch('http://www.omdbapi.com/?apikey=62eda7be&t=Star+wars&y=1977');
         const response = await data.json();
-        return console.log(response);
+        setMovieDetail(response);
       } catch (err) {
         console.log(err)
       }
@@ -21,7 +22,9 @@ const DetailsScreen = ({ navigation, route }) => {
   console.log(route)
   return (
     <View style={styles.mainView}>
-
+      <Text>{movieDetail ? movieDetail.Title : ''}</Text>
+      <Text>{movieDetail ? movieDetail.Year : ''}</Text>
+      <Text>{movieDetail ? movieDetail.Plot : ''}</Text>
     </View>
   )
 };
